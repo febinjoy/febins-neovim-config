9cba554970f02246f1933246e22d43968250228b
Febin Joy <febinjoy@gmail.com>
2024-10-29 21:53:13 +1100

    Add support for DAP Python

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -5,14 +5,17 @@ return {
     "rcarriga/nvim-dap-ui",
     "nvim-neotest/nvim-nio",
     "leoluz/nvim-dap-go",
+    "mfussenegger/nvim-dap-python",
   },
   config = function()
     local dap = require("dap")
     local dapui = require("dapui")
     local dapgo = require("dap-go")
+    local dappython = require("dap-python")
 
     dapui.setup()
     dapgo.setup()
+    dappython.setup()
 
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
