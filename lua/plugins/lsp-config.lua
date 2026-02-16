80cda51190bf6ee3bdbab4508e0dbede5ffb9d45
Febin Joy <febinjoy@gmail.com>
2026-02-17 09:04:41 +1100

    Refactor LSP configuration and error handlinglsp-config updated to support new version of neovim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,70 +1,39 @@
 -- All LSP related config resides here
 return {
-  {
+  "neovim/nvim-lspconfig",
+  dependencies = {
     "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "ts_ls",
-          "sqlls",
-          "bashls",
-          "gopls",
-          "cssls",
-          "html",
-          "lemminx", -- XML
-          "graphql",
-          "csharp_ls",
-        },
-      })
-    end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  config = function()
+    local status_mason, mason = pcall(require, "mason")
+    local status_mlsp, mason_lspconfig = pcall(require, "mason-lspconfig")
+    local status_lspconfig, lspconfig = pcall(require, "lspconfig")
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
+    if not (status_mason and status_mlsp and status_lspconfig) then
+      print("LSP Dependencies not found. Run :Lazy sync")
+      return
+    end
+
+    mason.setup()
+
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "lua_ls", "pyright", "ts_ls", "sqlls", "bashls",
+        "gopls", "cssls", "html", "lemminx", "graphql", "omnisharp"
+      },
+    })
+
+    -- This is the specific part that keeps failing
+    -- We verify the function exists before calling it
+    if mason_lspconfig.setup_handlers then
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({})
+        end,
       })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.sqlls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lemminx.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.graphql.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.csharp_ls.setup({
-        capabilities = capabilities,
-      })
-    end,
-  },
+    else
+      print("Error: setup_handlers is still nil. Try deleting the plugin folder.")
+    end
+  end,
 }
