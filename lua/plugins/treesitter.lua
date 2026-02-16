ba933711c4e9f3cf271b792d2ef5dba624ddd885
Febin Joy <febinjoy@gmail.com>
2026-02-17 09:07:42 +1100

    Refactor Treesitter setup with error handling for new versions of neovim
    Updated Treesitter configuration to handle module structure changes and added recommended parsers.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,25 @@
--- Tree Sitter
+-- Treesitter
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local treesitterconfigs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        -- Use pcall to avoid crashing if the module structure changes again
+        local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+        if not status_ok then
+            -- Fallback: some newer versions use the main module directly
+            configs = require("nvim-treesitter")
+        end
 
-		treesitterconfigs.setup({
-			auto_install = true,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+        configs.setup({
+            auto_install = true,
+            sync_install = false,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+            -- It is highly recommended to have these basic parsers
+            ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        })
+    end,
 }
