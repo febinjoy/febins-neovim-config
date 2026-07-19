3c7e7f4319478fee3d8cbf8cb70ae7b766222bd9
Febin Joy <febinjoy@gmail.com>
2026-07-19 18:38:29 +1000

    Add csharp debugging support

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,33 +1,36 @@
 -- Debuggers
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
-    "leoluz/nvim-dap-go",
-    "mfussenegger/nvim-dap-python",
-  },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-    local dapgo = require("dap-go")
-    local dappython = require("dap-python")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"leoluz/nvim-dap-go",
+		"mfussenegger/nvim-dap-python",
+		"NicholasMata/nvim-dap-cs",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		local dapgo = require("dap-go")
+		local dappython = require("dap-python")
+		local dapcs = require("dap-cs")
 
-    dapui.setup()
-    dapgo.setup()
-    dappython.setup()
+		dapui.setup()
+		dapgo.setup()
+		dappython.setup()
+		dapcs.setup()
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
-  end,
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+	end,
 }
